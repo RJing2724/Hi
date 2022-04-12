2a2a0ae09e0e4554a65ad6a6b7d4dd0d10304c58
RJing2724 <roy.zhuangjingbo@yahoo.com>
2022-04-12 01:01:30 -0400

    Add new file

---
+++ Hi.py
@@ -0,0 +1,6 @@
+name = input('Hi there. Please enter your name：');
+print('Hi', name, 'How are you doing today?');
+#注意检查每行的结尾是否完整
+height_data = input('Please entre your height(m):');
+weight_data = input ('Please enter your weight(kg):');
+print('Height:', height_data,'m;' 'Weight:',weight_data, 'kg.');
